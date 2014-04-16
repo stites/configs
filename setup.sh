033a9f7306cc48d839cb8af88f3ce66d459f7b8b
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2014-04-16 19:53:19 +0000

    added a setup script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+case "$1" in
+  host)
+    # Your Start Code
+    # Gotta fix this one a little bit!
+    ;;
+  guest)
+    mv  ./.bash_profile  ~/.bash_profile
+    mv  ./.bashrc        ~/.bashrc
+    source ~/.bashrc
+    source ~/.bash_profile
+    echo 'PS1="[\e[34;1m\u\[\e[0;34m\]@\H \[\e[1;30m\]\W\e[0m]\$ "' >> ~/.bashrc
+    curl http://j.mp/spf13-vim3 -L -o - | sh 
+    src
+    ;;
+  *)
+    echo "Usage: $0 {guest|host}" >&2
+    exit 1
+    ;;
+esac
