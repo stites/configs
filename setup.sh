b4edcde75079df7516f8f3893de62fd3af153f95
Sam <sam@stites.io>
2014-04-17 03:54:24 +0000

    modularized setup

--- setup.sh
+++ setup.sh
@@ -1,19 +1,23 @@
-pkgmanager=$(uname -a | grep ubuntu && echo "apt-get" || echo "yum")
-os_type=$(uname -a | grep ubuntu && echo "ubuntu" || echo "centOS")
-sudo $pkgmanager update -y && sudo $pkgmanager upgrade -y
-sudo $pkgmanager install -y vim wget git
+#!/bin/bash
 
-#curl http://j.mp/spf13-vim3 -L -o - | sh 
-curl https://github.com/clvv/fasd/tarball/1.0.1 -L -o fasd_dir
-tar xvf fasd_dir
-cd clvv-fasd-4822024
-sudo make install
-cd ..
-rm -rf fasd_dir
-rm -rf clvv-fasd-4822024
+function init {
+  pkgmanager=$(uname -a | grep ubuntu && echo "apt-get" || echo "yum")
+  os_type=$(uname -a | grep ubuntu && echo "ubuntu" || echo "centOS")
+  sudo $pkgmanager update -y && sudo $pkgmanager upgrade -y
+  sudo $pkgmanager install -y vim wget git
 
-cp -f ./.bash_profile  ~/.bash_profile
-cp -f ./.bashrc        ~/.bashrc
+  curl http://j.mp/spf13-vim3 -L -o - | sh 
+  curl https://github.com/clvv/fasd/tarball/1.0.1 -L -o fasd_dir
+  tar xvf fasd_dir
+  cd clvv-fasd-4822024
+  sudo make install
+  cd ..
+  rm -rf fasd_dir
+  rm -rf clvv-fasd-4822024
+
+  cp -f ./.bash_profile  ~/.bash_profile
+  cp -f ./.bashrc        ~/.bashrc
+}
 
 case "$1" in
   host)
@@ -21,6 +25,7 @@ case "$1" in
     # Gotta fix this one a little bit!
     ;;
   guest)
+    init
     echo 'PS1="${NORMAL}[${BRIGHT_BLUE}@${BLUE}${os_type}${NORMAL}] ${RESET}"' >> ~/.bashrc
     ;;
   *)
