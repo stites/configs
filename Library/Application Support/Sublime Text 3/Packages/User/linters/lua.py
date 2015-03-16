55c416131561d6a6a733c94eaca8ba880e1e1217
stites <sam@stites.io>
2015-03-16 12:04:16 -0700

    rm

--- Library/Application Support/Sublime Text 3/Packages/User/linters/lua.py
+++
@@ -1,9 +0,0 @@
-from lint import Linter
-
-class Lua(Linter):
-    language = ('coronasdklua', 'lua')
-    cmd = ('luac', '-p')
-    regex = '^luac: [^:]+:(?P<line>\d+): (?P<error>.+)'
-
-    def run(self, cmd, code):
-        return self.tmpfile(cmd, code, suffix='.lua')
