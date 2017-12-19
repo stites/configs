e6b31cbef23fbd8e67ec0117288db8fbe2fceb32
stites <sam@stites.io>
2017-12-19 12:24:49 -0500

    revamp super user sparks

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# provision folders (mkdir -p always succeeds)
+mkdir -p ~/.tmux/plugins
+mkdir -p ~/.config/nvim/autoload
+mkdir -p ~/.bash/log
+
 function install_fasd {
   echo "install fasd?"
   # prompt
@@ -12,27 +17,38 @@ function install_fasd {
   rm -rf clvv-fasd-4822024
 }
 
-
 function pkg_managers {
-  pkgmanager=$(uname -a | grep ubuntu && echo "apt-get" || echo "yum")
-  os_type=$(uname -a | grep ubuntu && echo "ubuntu" || echo "centOS")
-  sudo $pkgmanager update -y && sudo $pkgmanager upgrade -y
-  sudo $pkgmanager install -y vim wget git
+  case "$(uname -a)" in
+    *"Ubuntu"*)
+      echo "hello Ubuntu!"	
+      sudo apt update -y && sudo apt upgrade -y
+      sudo apt install fasd
+      sudo apt install vim wget git neovim curl
+      ;;
+    *"ARCH"*)
+      echo "hello Archlinux!"	
+      sudo aura -Ay
+      sudo aura -A fasd-git
+      sudo pacman -S vim wget git neovim
+      ;;
+    *"FreeBSD"*)
+      echo "hello FreeBSD!"	
+      sudo pkg update && sudo pkg upgrade
+      sudo pkg install vim wget git neovim
+      ;;
+  esac
 }
 
-case "$1" in
-  host)
-    # Your Start Code
-    # missing!
-    ;;
-  guest)
-    pkg_managers
-    install_fasd
-    echo 'PS1="${NORMAL}[${BRIGHT_BLUE}\u${BLUE}@${os_type}${NORMAL}|${BRIGHT_BLACK}\W${NORMAL}] ${RESET}"' >> ~/.bashrc
-    ;;
-  *)
-    echo "Usage: $0 {guest|host}" >&2
-    ;;
-esac
+if [ ! -d "$HOME/.tmuxifier" ]; then
+  git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
+fi
 
-source ~/.bashrc
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+if [ ! -f "$HOME/.config/nvim/autoload/plug.vim" ]; then
+  curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+pkg_managers
