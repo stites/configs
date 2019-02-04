fef08041a1802c40d04cdb1f96f2d13dd765a7a7
Sam Stites <sam@stites.io>
2019-02-04 15:00:33 -0500

    add hie to cachix

--- nixos/cachix.nix
+++ nixos/cachix.nix
@@ -4,9 +4,11 @@
   nix.binaryCaches = [
       "https://cache.nixos.org/"
       "https://nix-linter.cachix.org"
+      "https://hie-nix.cachix.org"
   ];
   nix.binaryCachePublicKeys = [
       "nix-linter.cachix.org-1:BdTne5LEHQfIoJh4RsoVdgvqfObpyHO5L0SCjXFShlE="
+      "hie-nix.cachix.org-1:EjBSHzF6VmDnzqlldGXbi0RM3HdjfTU3yDRi9Pd0jTY="
   ];
 }
 
