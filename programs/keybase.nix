c62b3e96ab87f69abd0315ef25237ff65512d0d3
Sam Stites <stites@users.noreply.github.com>
2019-08-30 13:16:16 -0400

    add keybase box name script

--- programs/keybase.nix
+++ programs/keybase.nix
@@ -5,4 +5,13 @@
   services.kbfs.mountPoint = "keybase"; # default: "keybase". Folder relative to HOME
   services.kbfs.extraFlags = [ ]; # default: [].
   home.packages = [ pkgs.keybase-gui ];
+  xdg.dataFile."keybase-newbox" = {
+    executable = true;
+    target = "../bin/keybase-newbox";
+    text = ''
+      #!/usr/bin/env bash
+      echo ''${HOSTNAME}-$(date +%s)
+    '';
+  };
+
 }
