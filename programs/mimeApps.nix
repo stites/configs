c57904bcdff8aa09991f7dc5ecc778b0a04ce8d0
Sam Stites <stites@users.noreply.github.com>
2019-10-06 20:03:15 -0400

    add mimeapps

---
+++ programs/mimeApps.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+
+let
+  browser = [
+    "firefox.desktop" # installed by home-manager, overlay turns this into devedition
+    # "firefox-devedition.desktop" # installed by home-manager
+    # "userapp-Firefox Developer Edition-4Z1P8Z.desktop" # installed by nixos-rebuild
+  ];
+  associations = {
+    "application/pdf" = [ "zathura" ];
+    "text/html" = browser;
+    "x-scheme-handler/http" = browser;
+    "x-scheme-handler/https" = browser;
+    "x-scheme-handler/ftp" = browser;
+    "x-scheme-handler/chrome" = browser;
+    "x-scheme-handler/about" = browser;
+    "x-scheme-handler/unknown" = browser;
+    "application/x-extension-htm" = browser;
+    "application/x-extension-html" = browser;
+    "application/x-extension-shtml" = browser;
+    "application/xhtml+xml" = browser;
+    "application/x-extension-xhtml" = browser;
+    "application/x-extension-xht" = browser;
+  };
+in
+{
+  xdg.mimeApps.enable = true;
+  xdg.mimeApps.associations.added = associations;
+  xdg.mimeApps.defaultApplications = associations;
+}
